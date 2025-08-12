7290702b06ef891884fe510e766efc53120e541c
NellitG <kamaunelson157@gmail.com>
2025-08-12 15:42:42 +0300

    Modify contact controller to serve proper HTML page

--- simple-blog/app/Http/Controllers/ContactController.php
+++ simple-blog/app/Http/Controllers/ContactController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     public function contact(){
-        return 'Contact us at example@gmail.com';
+        return view('contact');
     }
 }
