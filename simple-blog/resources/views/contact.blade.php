d0c4a00fbc42a85c6072fc0ce07b2d7255ab38bd
NellitG <kamaunelson157@gmail.com>
2025-08-12 15:49:40 +0300

    Create a simple contact template

---
+++ simple-blog/resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>contact</title>
+</head>
+<body>
+    <h1>Contact Us</h1>
+    <p>Contact us at nellit@gmail.com</p>
+    <p>Feel free to reach out with any questions or feedback.</p>
+</body>
+</html>
