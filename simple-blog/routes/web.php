0eabe7f49ff81b852259fc58f36758922b71b6bf
NellitG <kamaunelson157@gmail.com>
2025-08-13 10:53:08 +0300

    register home to route the fetch

--- simple-blog/routes/web.php
+++ simple-blog/routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\PostController;
 
+Route::get('/home', [PostController::class, 'index']);
 Route::get('/about', [PageController::class, 'about']);
 Route::get('/contact', [ContactController::class, 'contact']);
 
