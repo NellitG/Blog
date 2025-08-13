12fefa550a4be168a5ff2fbbdb678ec4fdf13b4e
NellitG <kamaunelson157@gmail.com>
2025-08-13 09:34:33 +0300

    Modify about to extend the layout page

--- simple-blog/resources/views/about.blade.php
+++ simple-blog/resources/views/about.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About - My Blog</title>
-</head>
-<body>
+@extends('layout')
+
+@section('title', 'About')
+
+@section('content')
     <h1>About My Blog</h1>
     <p>This blog is a platform where I share my thoughts, experiences, and insights on various topics. My goal is to create engaging content that resonates with readers and sparks meaningful discussions.</p>
-</body>
-</html>
+@endsection
