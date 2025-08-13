be78dcf7126dad8e0dc300716bdaa7e82467b244
NellitG <kamaunelson157@gmail.com>
2025-08-13 11:06:28 +0300

    create post controller to show all blogs stored in the database

---
+++ simple-blog/app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        // Fetch all posts from the database
+        $posts = Post::all();
+
+        //pass the posts to the view
+        return view('home', ['posts' => $posts]);
+    }
+}
