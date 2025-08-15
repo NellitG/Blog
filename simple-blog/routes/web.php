b3e0bb150d0470cfb47d74a1f32dfbffd8079ce4
NellitG <kamaunelson157@gmail.com>
2025-08-15 09:57:35 +0300

    Updated routes to include create and store actions

--- simple-blog/routes/web.php
+++ simple-blog/routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PostController;
 
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts/', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 Route::get('/home', [PostController::class, 'index']);
