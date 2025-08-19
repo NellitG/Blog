7635f7b3fc8c804d86735d1ac9c85ab91f6ec7d3
NellitG <kamaunelson157@gmail.com>
2025-08-19 10:02:10 +0300

    Add edit and update route

--- simple-blog/routes/web.php
+++ simple-blog/routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\PostController;
 
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/posts/', [PostController::class, 'store'])->name('posts.store');
