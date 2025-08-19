6e53cad2eeaa610d9bbf66693875a2e566710436
NellitG <kamaunelson157@gmail.com>
2025-08-19 10:08:53 +0300

    Add category_id column to posts table

--- simple-blog/database/migrations/2025_08_18_100928_add_category_id_to_posts_table.php
+++ simple-blog/database/migrations/2025_08_18_100928_add_category_id_to_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->unsignedBigInteger('category_id')->nullable();
         });
     }
 
